3aeab633637c9f7f0bd5a50b4a6d826fe0c5a11b
Mubtasim <mubtasimf443@gmail.com>
2025-04-13 20:27:00 +0600

    oTHERS CATAGORY ADDED

--- server/data/products/Brands/Others.ts
+++ server/data/products/Brands/Others.ts
@@ -1,6 +1,62 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah
 */
-import { IBrands } from '../Brands';;
+import { IBrands } from '../Brands';
 
-export const OthersBrands :IBrands[] = [];
+export const OthersBrands :IBrands[] = [
+    // Industrial & Construction
+    {
+        id: 1560,
+        name: "RFL (Construction)",
+        slug: "rfl-construction",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1500, 1501],
+        parentProductTypes: [1510, 1520]
+    },
+    {
+        id: 1561,
+        name: "PHP (Steel)",
+        slug: "php-steel",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1501],
+        parentProductTypes: [1521]
+    },
+
+    // Agriculture
+    {
+        id: 1570,
+        name: "ACI (Agriculture)",
+        slug: "aci-agriculture",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1502],
+        parentProductTypes: [1530, 1531]
+    },
+
+    // Office & Technology
+    {
+        id: 1580,
+        name: "Brother",
+        slug: "brother",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1503],
+        parentProductTypes: [1540]
+    },
+
+    // Security & Services
+    {
+        id: 1590,
+        name: "Hikvision",
+        slug: "hikvision",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1505],
+        parentProductTypes: [1550]
+    },
+    {
+        id: 1591,
+        name: "Local Educational Institutions",
+        slug: "local-educational-institutions",
+        parentPrimeCategoryIds: [15],
+        parentSubCategoryIds: [1507],
+        parentProductTypes: [1551]
+    }
+];
--- server/data/products/ProductTypes/Others.ts
+++ server/data/products/ProductTypes/Others.ts
@@ -1,7 +1,86 @@
 /*
 بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah
 */
+import { IProductTypes } from '../ProductTypes';
 
-import { IProductTypes } from '../ProductTypes';;
+export const OthersProductTypes :IProductTypes[] = [
+    // Industrial Supplies
+    {
+        id: 1510,
+        name: "Welding Equipment",
+        slug: "welding-equipment",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1500
+    },
+    {
+        id: 1511,
+        name: "Generators",
+        slug: "generators",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1500
+    },
 
-export const OthersProductTypes :IProductTypes[] = [];
+    // Construction Materials
+    {
+        id: 1520,
+        name: "Cement",
+        slug: "cement",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1501
+    },
+    {
+        id: 1521,
+        name: "Steel Rods",
+        slug: "steel-rods",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1501
+    },
+
+    // Agriculture
+    {
+        id: 1530,
+        name: "Fertilizers",
+        slug: "fertilizers",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1502
+    },
+    {
+        id: 1531,
+        name: "Agricultural Tools",
+        slug: "agricultural-tools",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1502
+    },
+
+    // Office & Religious
+    {
+        id: 1540,
+        name: "Office Chairs",
+        slug: "office-chairs",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1503
+    },
+    {
+        id: 1541,
+        name: "Prayer Mats",
+        slug: "prayer-mats",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1504
+    },
+
+    // Security & Services
+    {
+        id: 1550,
+        name: "CCTV Cameras",
+        slug: "cctv-cameras",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1505
+    },
+    {
+        id: 1551,
+        name: "Online Courses",
+        slug: "online-courses",
+        parentPrimeCategoryId: 15,
+        parentSubCategoryId: 1507
+    }
+];
