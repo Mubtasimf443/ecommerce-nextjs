8a9eaf586e8cd4293cc89d938ba8272d2d6b6018
Mubtasim <mubtasimf443@gmail.com>
2025-04-26 11:38:40 +0600

    notification component initial creation file establish;

--- client/src/app/(admin_panal)/web-admin/[[...slug]]/_components/sections/notifications/NotificationsSection.tsx
+++ client/src/app/(admin_panal)/web-admin/[[...slug]]/_components/sections/notifications/NotificationsSection.tsx
@@ -1,15 +1,28 @@
 /* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
-
+"use client"
+import { useParams } from 'next/navigation';
 import React, { FC, Fragment } from 'react';
+import NotificationList from './NotificationList';
+import NotificationHeader from './NotificationHeader';
+import NotificationListFilter from './NotificationListFilter';
+import CreateNotificationCard from './CreateNotificationCard';
 
 interface Props {
 
 };
 
 const NotificationsSection :FC<Props> = ({}) => {
+  let {slug} = useParams();
   return (
     <Fragment>
-      
+      <NotificationHeader />
+  
+      {(slug[1] === undefined || slug[1]?.toLowerCase() === 'list') && <NotificationListFilter />}
+
+      {(slug[1] === undefined || slug[1].toLowerCase() === 'list') && <NotificationList />}
+
+      {slug[1]?.toLowerCase() === 'create' && <CreateNotificationCard />}
+
     </Fragment>
   );
 };
