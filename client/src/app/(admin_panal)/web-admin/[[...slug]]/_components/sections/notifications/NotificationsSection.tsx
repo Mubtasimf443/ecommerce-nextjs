a8b9b78a9089128a982d1c6f315c7b1bb5466b8b
Mubtasim <mubtasimf443@gmail.com>
2025-04-26 11:50:25 +0600

    notification section organized;

--- client/src/app/(admin_panal)/web-admin/[[...slug]]/_components/sections/notifications/NotificationsSection.tsx
+++ client/src/app/(admin_panal)/web-admin/[[...slug]]/_components/sections/notifications/NotificationsSection.tsx
@@ -1,23 +1,49 @@
 /* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ ﷺ InshaAllah */
 "use client"
 import { useParams } from 'next/navigation';
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useRef } from 'react';
 import NotificationList from './NotificationList';
 import NotificationHeader from './NotificationHeader';
 import NotificationListFilter from './NotificationListFilter';
 import CreateNotificationCard from './CreateNotificationCard';
+import TabsWithoutContent, { TabsType } from '../../layout/TabsWithoutContent';
 
 interface Props {
 
 };
 
-const NotificationsSection :FC<Props> = ({}) => {
-  let {slug} = useParams();
+const NotificationsSection: FC<Props> = ({ }) => {
+  let { slug } = useParams();
+  const tabs: TabsType = [
+    { title: 'History', value: 'history' },
+    { title: "Create", value: "create" }
+  ];
+
+  let selectedTab = useRef<string>(slug[0]?.toLowerCase() || "list");
+  if (selectedTab.current != 'list' &&selectedTab.current != 'create'  ) {
+    return (
+      <>
+      
+      </>
+    );
+  }
+  function tabChange(tab:string) {
+    
+  }
+
   return (
     <Fragment>
       <NotificationHeader />
-  
+
       {(slug[1] === undefined || slug[1]?.toLowerCase() === 'list') && <NotificationListFilter />}
+
+      <TabsWithoutContent
+        tabs={tabs}
+        defaultValue={selectedTab.current }
+        onValueChange={(tab) => {
+
+        }}
+      />
 
       {(slug[1] === undefined || slug[1].toLowerCase() === 'list') && <NotificationList />}
 
